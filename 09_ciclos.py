36cc8b77bdc500c6c28f7361d7eb7ededc3ac507
Elandy <andersonramirezcampos@gmail.com>
2026-01-09 01:33:42 -0500

    ciclos

---
+++ 09_ciclos.py
@@ -0,0 +1,18 @@
+#Ciclos#
+
+#While=mientras seas verdadero haz algo
+
+mi_condicion = 0
+
+while mi_condicion < 10:
+    print(mi_condicion)
+    mi_condicion += 1
+
+print("continua")
+
+lista = [23,12,31,42,35,65,13]
+#Se repite tantas veces cuantos elementos tenga
+for elemento in lista:
+    print(elemento)
+
+print("continua")
